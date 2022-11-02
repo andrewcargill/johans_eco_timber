4d190098c313c9c96a6cf6cfc3b5f6a4f8180263
andrewcargill <andycargill01@aol.com>
2022-11-02 08:47:22 +0000

    Update settings allowed hosts

--- timber_company/settings.py
+++ timber_company/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["timber_company.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["timbercompany.herokuapp.com", "localhost"]
 
 
 # Application definition
