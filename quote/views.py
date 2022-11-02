bc8d46a27de6f36d9e83e53114f68911638e3d64
andrewcargill <andycargill01@aol.com>
2022-11-02 12:31:43 +0000

    Create working homepage boilerplate

--- quote/views.py
+++ quote/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Quote, Item
 
-# Create your views here.
+class QuoteList(generic.ListView):
+    model = Quote
+    queryset = Quote.objects.order_by('-submitted_date')
+    template_name = 'index.html'
+    paginate_by = 6
