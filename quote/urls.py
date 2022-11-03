a795bb88e1b423d2e4fefe0c4eebb3de4074383f
andrewcargill <andycargill01@aol.com>
2022-11-03 12:06:30 +0000

    Display template for user screen after login

--- quote/urls.py
+++ quote/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.QuoteList.as_view(), name='home'),
+    path('user_home3.html', views.QuoteList.as_view(), name='user_home'),
+    path('user_home.html', views.User.as_view(), name='user_home'),
 ]
--- quote/views.py
+++ quote/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views import generic, View
 from .models import Quote, Item
+from django.contrib.auth.models import User
 
 class QuoteList(generic.ListView):
     model = Quote
     queryset = Quote.objects.order_by('-submitted_date')
     template_name = 'index.html'
     paginate_by = 6
+
+class User(generic.ListView):
+    model = User
+    queryset = User.objects
+    template_name = 'user_home.html'
