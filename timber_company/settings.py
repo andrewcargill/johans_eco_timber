65ee4e3f49efab57a6a64fa6fbb4469eb566c045
andrewcargill <andycargill01@aol.com>
2022-11-03 07:15:32 +0000

    Enable user login via /accounts/signup

--- timber_company/settings.py
+++ timber_company/settings.py
@@ -45,11 +45,22 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'quote',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- timber_company/urls.py
+++ timber_company/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quote.urls'), name='quote_urls'),
+    path("accounts/", include("allauth.urls")),
 ]
