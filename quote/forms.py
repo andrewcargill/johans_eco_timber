eed47214c96ef8430e546cf445ae2b86b24c1615
andrewcargill <andycargill01@aol.com>
2022-11-04 08:49:32 +0000

    Add form template and get request to user_home

--- quote/forms.py
+++ quote/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
-class QuoteForm(forms.Form):
-    title = forms.CharField()
+# class QuoteForm(forms.Form):
+#     text = forms.CharField()
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
--- quote/views.py
+++ quote/views.py
@@ -3,7 +3,9 @@ from django.views import generic, View
 from django.views.generic import TemplateView
 from .models import Quote, Item
 from django.contrib.auth.models import User
-from .forms import QuoteForm
+from .forms import NameForm
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
 
 
 class QuoteList(generic.ListView):
@@ -12,16 +14,38 @@ class QuoteList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 6
 
+
+def get_name(request):
+    print("-------GETTING CALLED")
+    print(request.method)
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        print("-------POST is True")
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            print("-------form is valid")
+            return HttpResponseRedirect('/home/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        print("-------form is NOT valid")
+        form = NameForm()
+
+    return render(request, 'user_home.html', {'form': form})
+
+
+
 # class User(generic.ListView):
 #     model = User
 #     queryset = User.objects
 #     template_name = 'user_home.html'
 
-class User(TemplateView):
-    model = User
-    queryset = User.objects
-    template_name = 'user_home.html'
 
-    def get(self, request):
-        form = QuoteForm()
-        return render(request, self.template_name, {'form': form})
+
+
+
