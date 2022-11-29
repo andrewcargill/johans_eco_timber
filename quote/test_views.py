cd7de1dfff2a4b9fcc36aa4cbed14c362c06042f
andrewcargill <andycargill01@aol.com>
2022-11-29 09:41:50 +0000

    Add view test

--- quote/test_views.py
+++ quote/test_views.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
 
+
 class TestViews(TestCase):
 
     def test_home_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_about_page(self):
+        response = self.client.get('about')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
